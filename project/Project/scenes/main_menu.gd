89b6e416b8db42e336a2404bdc6d974fdd8d62f8
EmiThomson92 <emithomson92@gmail.com>
2024-02-29 17:51:25 +0000

    Added Menu Animation

--- project/Project/scenes/main_menu.gd
+++ project/Project/scenes/main_menu.gd
@@ -1,4 +1,6 @@
 extends Node2D
+@onready var dragon_1 = $CenterContainer/Container/Dragon1
+@onready var dragon_2 = $CenterContainer/Container2/Dragon1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,4 +17,16 @@ func _on_texture_button_pressed():
 	print_debug("CLICK: "+str(GlobalControl.level))
 	GlobalControl.level += 1
 	GlobalControl.goto_scene("res://scenes/playable_levels/level_1.tscn")
+	pass # Replace with function body.
+
+
+func _on_texture_button_mouse_entered():
+	dragon_1.frame = 1
+	dragon_2.frame = 1
+	pass # Replace with function body.
+
+
+func _on_texture_button_mouse_exited():
+	dragon_1.frame = 0
+	dragon_2.frame = 0
 	pass # Replace with function body.
