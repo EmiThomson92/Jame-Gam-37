79154a5a495db4ee96b33e87048c6176e2e717b8
EmiThomson92 <emithomson92@gmail.com>
2024-03-01 11:40:32 +0000

    Gallery Basic Added

--- project/Project/scenes/main_menu.gd
+++ project/Project/scenes/main_menu.gd
@@ -15,21 +15,31 @@ func _process(delta):
 	pass
 
 
-func _on_texture_button_pressed():
+func _on_start_button_pressed():
 	print_debug("CLICK: "+str(GlobalControl.level))
 	GlobalControl.goto_scene("res://scenes/playable_levels/level_1.tscn")
 	pass # Replace with function body.
 
+func _on_gallery_button_pressed():
+	print_debug("CLICK: "+str(GlobalControl.level))
+	GlobalControl.goto_scene("res://scenes/playable_levels/gallery.tscn")
+	pass # Replace with function body.
+	
+func _on_credits_button_pressed():
+	print_debug("CLICK: "+str(GlobalControl.level))
+	GlobalControl.goto_scene("res://scenes/playable_levels/level_1.tscn")
+	pass # Replace with function body.
 
-func _on_texture_button_mouse_entered():
+func _on_button_mouse_entered():
 	dragon_1.frame = 1
 	dragon_2.frame = 1
 	portal.frame = 1
 	pass # Replace with function body.
 
 
-func _on_texture_button_mouse_exited():
+func _on_button_mouse_exited():
 	dragon_1.frame = 0
 	dragon_2.frame = 0
 	portal.frame = 0
 	pass # Replace with function body.
+
---
+++ project/Project/scenes/playable_levels/gallery.gd
@@ -0,0 +1,28 @@
+extends Node2D
+var gallery_sprite = {}
+@onready var gallery_display = $CenterContainer/Control/gallery_display
+var current_sprite = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	gallery_sprite[0] = "player_idle"
+	gallery_sprite[1] = "player_walk"
+	gallery_sprite[2] = "player_jump"
+	gallery_sprite[3] = "player_hurt"
+	gallery_sprite[4] = "BG_1"
+	gallery_sprite[5] = "BG_2"
+	gallery_display.play(gallery_sprite[current_sprite])
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	gallery_display.play(gallery_sprite[current_sprite])
+	pass
+
+
+func _on_right_button_pressed():
+	print_debug(gallery_sprite.size())
+	current_sprite += 1
+	if current_sprite > gallery_sprite.size()-1:
+		current_sprite = 0
+	pass # Replace with function body.
