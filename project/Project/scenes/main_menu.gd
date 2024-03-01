845947138915675ed20ce59f55417c5d0aab45b1
EmiThomson92 <emithomson92@gmail.com>
2024-03-01 09:12:10 +0000

    Adjusted window sizing

--- project/Project/scenes/main_menu.gd
+++ project/Project/scenes/main_menu.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var dragon_1 = $CenterContainer/Container/Dragon1
 @onready var dragon_2 = $CenterContainer/Container2/Dragon1
+@onready var portal = $CenterContainer/Portal
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,10 +24,12 @@ func _on_texture_button_pressed():
 func _on_texture_button_mouse_entered():
 	dragon_1.frame = 1
 	dragon_2.frame = 1
+	portal.frame = 1
 	pass # Replace with function body.
 
 
 func _on_texture_button_mouse_exited():
 	dragon_1.frame = 0
 	dragon_2.frame = 0
+	portal.frame = 0
 	pass # Replace with function body.
